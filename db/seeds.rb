dc601245b550c2e832e271a50f31c3c5d0458f15
Kylee Acker <kyleeacker@Kylees-MacBook-Air.local>
2017-04-18 17:19:29 -0400

    dumb

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,121 +46,121 @@ states = [
   "Texas",
   "Utah",
   "Vermont",
-  "Virginia",
-  "Washington",
-  "West Virginia",
-  "Wisconsin",
-  "Wyoming",
-]
-state_converter = {
-  "AL" => 'Alabama',
-  'AK' => "Alaska",
-  "AZ" => 'Arizona',
-  'AR' => "Arkansas",
-  "CA" => 'California',
-  'CO' => "Colorado",
-  "CT" => 'Connecticut',
-  'DE' => "Delaware",
-  "FL" => 'Florida',
-  'GA' => "Georgia",
-  "HI" => 'Hawaii',
-  'ID' => "Idaho",
-  "IL" => 'Illinois',
-  'IN' => "Indiana",
-  "IA" => 'Iowa',
-  'KS' => "Kansas",
-  "KY" => 'Kentucky',
-  'LA' => "Louisiana",
-  "ME" => 'Maine',
-  'MD' => "Maryland",
-  "MA" => 'Massachusetts',
-  'MI' => "Michigan",
-  "MN" => 'Minnesota',
-  'NE' => "Nebraska",
-  "NV" => 'Nevada',
-  'NM' => "New Hampshire",
-  "NJ" => 'New Jersey',
-  'NM' => "New Mexico",
-  "NY" => 'New York',
-  'NC' => "North Carolina",
-  "ND" => 'North Dakota',
-  'OH' => "Ohio",
-  "OK" => 'Oklahoma',
-  'OR' => "Oregon",
-  "PA" => 'Pennsylvania',
-  'RI' => "Rhode Island",
-  "SC" => 'South Carolina',
-  'SD' => "South Dakota",
-  "TN" => 'Tennessee',
-  'TX' => "Texas",
-  "UT" => 'Utah',
-  'VT' => "Vermont",
-  "VA" => 'Virginia',
-  'WA' => "Washington",
-  'WV' => "West Virginia",
-  'WI' => "Wisconsin",
-  'WY' => "Wyoming"
-}
-
-
-words = ["economy", "women", "poverty", "business", "jobs", "health", "energy",
-  "workers", "climate", "equality", "families", "tax", "military",
-  "education", "immigration", "government", "children", "budget", "oil",
-  "class", "medicare", "welfare", "debt", "god", "regulation"]
-
-states.each do |name|
-  State.find_or_create_by(name: name)
-end
-
-# states.each do |state|
-# abbreviation = state_converter[state]
-
-words.each do |word|
-  url = "http://www.capitolwords.org/api/1/phrases/state.json?phrase=#{word}&page=0&per_page=50&sort=count&apikey=#{ENV["SUNLIGHT_KEY"]}"
-  results = HTTParty.get(url)["results"]
-
-  results.each do |hash|
-    state = State.find_by(name: state_converter[hash["state"]])
-    StateWordCount.find_or_create_by(state_id: state.id, count: hash["count"], word: word) if state
-  end
-end
-
-
-
-
-  100.times do
-    user = User.new(
-      username: Faker::Name.name,
-      email: Faker::Internet.email,
-      password: Faker::Internet.password,
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      affiliation: %w(Republican Democrat Independent Undecided).sample,
-      sign_in_count: 0
-    )
-    if user.valid?
-      user.save!
-    end
-  end
-
-  500.times do
-    petition = Petition.new(
-      name: "Petition",
-      description: Faker::Lorem.paragraph(2, false),
-      owner_id: rand(2..User.all.count),
-      state_id: rand(2..State.all.count),
-    )
-    if petition.valid?
-      petition.save!
-    end
-  end
-
-  5000.times do
-    signature = Membership.new(
-      user_id: rand(2..User.all.count),
-      petition_id: rand(2..Petition.all.count)
-    )
-    if signature.valid?
-      signature.save!
-    end
-  end
+#   "Virginia",
+#   "Washington",
+#   "West Virginia",
+#   "Wisconsin",
+#   "Wyoming",
+# ]
+# state_converter = {
+#   "AL" => 'Alabama',
+#   'AK' => "Alaska",
+#   "AZ" => 'Arizona',
+#   'AR' => "Arkansas",
+#   "CA" => 'California',
+#   'CO' => "Colorado",
+#   "CT" => 'Connecticut',
+#   'DE' => "Delaware",
+#   "FL" => 'Florida',
+#   'GA' => "Georgia",
+#   "HI" => 'Hawaii',
+#   'ID' => "Idaho",
+#   "IL" => 'Illinois',
+#   'IN' => "Indiana",
+#   "IA" => 'Iowa',
+#   'KS' => "Kansas",
+#   "KY" => 'Kentucky',
+#   'LA' => "Louisiana",
+#   "ME" => 'Maine',
+#   'MD' => "Maryland",
+#   "MA" => 'Massachusetts',
+#   'MI' => "Michigan",
+#   "MN" => 'Minnesota',
+#   'NE' => "Nebraska",
+#   "NV" => 'Nevada',
+#   'NM' => "New Hampshire",
+#   "NJ" => 'New Jersey',
+#   'NM' => "New Mexico",
+#   "NY" => 'New York',
+#   'NC' => "North Carolina",
+#   "ND" => 'North Dakota',
+#   'OH' => "Ohio",
+#   "OK" => 'Oklahoma',
+#   'OR' => "Oregon",
+#   "PA" => 'Pennsylvania',
+#   'RI' => "Rhode Island",
+#   "SC" => 'South Carolina',
+#   'SD' => "South Dakota",
+#   "TN" => 'Tennessee',
+#   'TX' => "Texas",
+#   "UT" => 'Utah',
+#   'VT' => "Vermont",
+#   "VA" => 'Virginia',
+#   'WA' => "Washington",
+#   'WV' => "West Virginia",
+#   'WI' => "Wisconsin",
+#   'WY' => "Wyoming"
+# }
+#
+#
+# words = ["economy", "women", "poverty", "business", "jobs", "health", "energy",
+#   "workers", "climate", "equality", "families", "tax", "military",
+#   "education", "immigration", "government", "children", "budget", "oil",
+#   "class", "medicare", "welfare", "debt", "god", "regulation"]
+#
+# states.each do |name|
+#   State.find_or_create_by(name: name)
+# end
+#
+# # states.each do |state|
+# # abbreviation = state_converter[state]
+#
+# words.each do |word|
+#   url = "http://www.capitolwords.org/api/1/phrases/state.json?phrase=#{word}&page=0&per_page=50&sort=count&apikey=#{ENV["SUNLIGHT_KEY"]}"
+#   results = HTTParty.get(url)["results"]
+#
+#   results.each do |hash|
+#     state = State.find_by(name: state_converter[hash["state"]])
+#     StateWordCount.find_or_create_by(state_id: state.id, count: hash["count"], word: word) if state
+#   end
+# end
+#
+#
+#
+#
+#   100.times do
+#     user = User.new(
+#       username: Faker::Name.name,
+#       email: Faker::Internet.email,
+#       password: Faker::Internet.password,
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       affiliation: %w(Republican Democrat Independent Undecided).sample,
+#       sign_in_count: 0
+#     )
+#     if user.valid?
+#       user.save!
+#     end
+#   end
+#
+#   500.times do
+#     petition = Petition.new(
+#       name: "Petition",
+#       description: Faker::Lorem.paragraph(2, false),
+#       owner_id: rand(2..User.all.count),
+#       state_id: rand(2..State.all.count),
+#     )
+#     if petition.valid?
+  #     petition.save!
+  #   end
+  # end
+  #
+  # 5000.times do
+  #   signature = Membership.new(
+  #     user_id: rand(2..User.all.count),
+  #     petition_id: rand(2..Petition.all.count)
+  #   )
+  #   if signature.valid?
+  #     signature.save!
+  #   end
+  # end
